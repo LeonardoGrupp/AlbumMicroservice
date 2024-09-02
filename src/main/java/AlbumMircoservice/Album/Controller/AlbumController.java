9cab4c48f95bd884f70785dd3eaca66b0c539417
Flowloss <kolipopan@gmail.com>
2024-09-02 11:33:17 +0200

    Basic empty classes

--- src/main/java/AlbumMircoservice/Album/Controller/AlbumController.java
+++ src/main/java/AlbumMircoservice/Album/Controller/AlbumController.java
@@ -1,7 +1,11 @@
 package AlbumMircoservice.Album.Controller;
 
+import AlbumMircoservice.Album.Repository.AlbumRepository;
+import AlbumMircoservice.Album.Service.AlbumService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AlbumController {
+
+    private AlbumService albumService;
 }
