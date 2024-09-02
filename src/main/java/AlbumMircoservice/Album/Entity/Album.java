59a9f445c67245c90f1e15dbddd087b869c5729c
Flowloss <kolipopan@gmail.com>
2024-09-02 11:32:41 +0200

    Basic empty classes

--- src/main/java/AlbumMircoservice/Album/Entity/Album.java
+++ src/main/java/AlbumMircoservice/Album/Entity/Album.java
@@ -1,4 +1,11 @@
 package AlbumMircoservice.Album.Entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "album")
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
--- src/main/java/AlbumMircoservice/Album/Repository/AlbumRepository.java
+++ src/main/java/AlbumMircoservice/Album/Repository/AlbumRepository.java
@@ -1,7 +1,8 @@
 package AlbumMircoservice.Album.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlbumRepository {
+public interface AlbumRepository extends JpaRepository {
 }
