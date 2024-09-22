456a1a140a47c135b00a4ea1d78c55dfcf740913
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-09-23 00:42:17 +0200

    commiting more albums

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,33 @@
 INSERT INTO album (album_name, release_date)
 VALUES
     -- Music
-    ('Come Clarity', '2006-02-07'),
-    ('Rated R', '2009-11-20'),
-    ('Good Girl Gone Bad', '2007-05-31'),
-    ('Rock in Rio', '2002'),
-    ('Alchemy', '1984'),
+    ('Come Clarity', '2006-02-07'), -- 1
+    ('Rated R', '2009-11-20'), -- 2
+    ('Good Girl Gone Bad', '2007-05-31'), -- 3
+
+    -- Videos
+    ('Rock in Rio', '2002'), -- 4
+    ('Alchemy', '1984'), -- 5
 
     -- POD
-    ('The Joe Rogan Experience', '2000'),
-    ('Lex Fridman Podcast', '2000'),
-    ('This Past Weekend', '2000')
+    ('The Joe Rogan Experience', '2000'), -- 6
+    ('Lex Fridman Podcast', '2000'), -- 7
+    ('This Past Weekend', '2000'), -- 8
+
+    -- Music
+    ('Voodoo', '2000-01-25'), -- 9
+    ('Channel Orange', '2012-07-10'), -- 10
+    ('The Miseducation of Lauryn Hill', '1998-08-19'), -- 11
+    ('Maxwell''s Urban Hang Suite', '1996-04-02'), -- 12
+    ('Whats Going On', '2000'), -- 13
+    ('The Electric Lady', '2000'), -- 14
+    ('Confessions', '2000'), -- 15
+    ('Get Lifted', '2000'), -- 16
+    ('Nurture', '2000'), -- 17
+    ('True Colors', '2000'), -- 18
+    ('True', '2000'), -- 19
+    ('Good Faith', '2000'), -- 20
+    ('Stories', '2000'), -- 21
+    ('Worlds', '2000'), -- 22
+    ('Kids in Love', '2000') -- 23
 ;
