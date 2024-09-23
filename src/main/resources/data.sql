304f1bf58d4c45c3e1608fd2a9d66296657531a4
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-09-23 21:20:03 +0200

    added 55+ albums

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,5 +29,41 @@ VALUES
     ('Good Faith', '2000'), -- 20
     ('Stories', '2000'), -- 21
     ('Worlds', '2000'), -- 22
-    ('Kids in Love', '2000') -- 23
+    ('Kids in Love', '2000'), -- 23
+    ('Paranoid', '2000'), -- 24
+    ('Master of Puppets', '2000'), -- 25
+    ('Reign in Blood', '2000'), -- 26
+    ('Vulgar Display of Power', '2000'), -- 27
+    ('Blizzard of Ozz', '2000'), -- 28
+    ('Screaming for Vengeance', '2000'), -- 29
+    ('Britney', '2001'), -- 30
+    ('Justified', '2000'), -- 31
+    ('The Fame', '2000'), -- 32
+    ('One of the Boys', '2001'), -- 33
+    ('Lemonade', '2000'), -- 34
+    ('1989', '2000'), -- 35
+    ('Four', '2000'), -- 36
+    ('Illmatic', '2000'), -- 37
+    ('Aquemini', '2001'), -- 38
+    ('All Eyez on Me', '2000'), -- 39
+    ('Get Rich or Die Tryin', '2000'), -- 40
+    ('The Low End Theory', '2000'), -- 41
+    ('Time Out', '2000'), -- 42
+    ('Blue Train', '2000'), -- 43
+    ('The Sidewinder', '2000'), -- 44
+    ('The Turnaround!', '2000'), -- 45
+    ('Moanin''', '2000'), -- 46
+    ('Everybody Digs Bill Evans', '2000'), -- 47
+    ('Revolver', '2000'), -- 48
+    ('The Dark Side of the Moon', '2000'), -- 49
+    ('Kid A', '2000'), -- 50
+    ('IV', '2000'), -- 51
+    ('The Rise and Fall of Ziggy Stardust and the Spiders from Mars', '2000'), -- 52
+    ('Rumours', '2000'), -- 53
+    ('Lovedrive', '1979'), -- 54
+    ('Overkill', '1979'), -- 55
+    ('Long Live Rock n Roll', '2000'), -- 56
+    ('Burn', '1974'), -- 57
+    ('Secret Treaties', '1974'), -- 58
+    ('Never Turn Your Back on a Friend', '1973') -- 59
 ;
